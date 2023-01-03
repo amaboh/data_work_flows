ccdec14f2fc20bcbdfbe4b5262851d348cde59f9
amaboh <amabocanon@gmail.com>
2023-01-03 16:59:47 +0000

    ingestion script updated

--- 2_docker_sql/ingest_data.py
+++ 2_docker_sql/ingest_data.py
@@ -18,16 +18,30 @@ def main(params):
     db = params.db
     table_name = params.table_name
     url = params.url
-    csv_name = 'output.csv'
 
+
+ # the backup files are gzipped, and it's important to keep the correct extension
+    # for pandas to be able to open the file
+    if url.endswith('.csv.gz'):
+        csv_name = 'output.csv.gz'
+    else:
+        csv_name = 'output.csv'
+
+    
     # download the CSV file
     os.system(f"wget {url} -O {csv_name}")
 
+    #for runing in Mac locally
+    #os.system(f"curl {url} -o {csv_name}")
+    #os.system("gunzip"+csv_name)
+
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+    # df_iter = pd.read_csv(csv_name, iternator=True, chucksize=100000)
 
     df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
 
     df = next(df_iter)
+   
 
     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
@@ -60,14 +74,16 @@ if __name__ == '__main__':
     # Parse the command line arguments and calls the main program
     parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
 
-    parser.add_argument('--user', help='user name for postgres')
-    parser.add_argument('--password', help='password for postgres')
-    parser.add_argument('--host', help='host for postgres')
-    parser.add_argument('--port', help='port for postgres')
-    parser.add_argument('--db', help='database name for postgres')
-    parser.add_argument('--table_name', help='name of the table where we will write the results to')
-    parser.add_argument('--url', help='url of the csv file')
+    parser.add_argument('--user', required=True, help='user name for postgres')
+    parser.add_argument('--password', required=True, help='password for postgres')
+    parser.add_argument('--host', required=True, help='host for postgres')
+    parser.add_argument('--port', required=True, help='port for postgres')
+    parser.add_argument('--db', required=True, help='database name for postgres')
+    parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
+    parser.add_argument('--url', required=True, help='url of the csv file')
 
     args = parser.parse_args()
 
     main(args)
+
+    
